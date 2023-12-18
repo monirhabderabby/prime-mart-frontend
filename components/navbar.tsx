6904b0d155ca000164926c3a2e1e9c4ecca31572
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2023-12-18 23:02:11 +0600

    Environment setup & featured products (Store) [completed]

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = async () => {
         <div className="border-b">
             <Container>
                 <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
-                    <Link href={"/"} className="ml-4 flex lg:ml-0 gap-x-2">
+                    <Link href={"/"} className="ml-4 flex lg:ml-0 gap-x-2 mr-6">
                         <p className="font-bold text-xl">Prime Mart</p>
                     </Link>
                     <MainNav data={categories} />
